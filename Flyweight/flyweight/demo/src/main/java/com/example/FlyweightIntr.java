8ac70b8669e9bbd0758ab4321911b69a8d0f7582
JuanCarrill0 <ju4nc4rrill00@gmail.com>
2025-05-08 12:15:36 -0500

    Flyweight diagramas y documentación

---
+++ Flyweight/flyweight/demo/src/main/java/com/example/FlyweightIntr.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public interface FlyweightIntr {
+  public String getNameFac();
+  public String getLocality();
+  public String getAddress();
+  public String getNeighbourhood();
+  public String getCoordinates();
+  public void print(String name, String code);
+}
